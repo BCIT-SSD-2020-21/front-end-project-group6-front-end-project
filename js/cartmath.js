986c74de3ac9b3c9b130902cc4ac6fac3c39d74c
scurrie90 <scurrie30@my.bcit.ca>
2020-10-22 16:14:19 -0700

    Wrote summation function for cartItems array

---
+++ js/cartmath.js
@@ -0,0 +1,17 @@
+
+let cartItem = []
+let itemPrices = []
+
+function sum(itemPrices) {
+    if (toString.call(itemPrices) !== "[object Array]")
+        return false;
+
+        var subTotal = 0
+        for (var i=0; i<itemPrices.length; i++)
+            {
+                if (isNaN(itemPrices[i])){
+                    continue;
+                }
+                subTotal += Number(itemPrices[i]);
+            }
+}

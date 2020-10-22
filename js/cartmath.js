7df309fd31aed62904b5242f994ac8475ec56e10
scurrie90 <scurrie30@my.bcit.ca>
2020-10-22 16:40:56 -0700

    Further functionality. Still buggy. Just outlining

--- js/cartmath.js
+++ js/cartmath.js
@@ -1,6 +1,20 @@
 
-let cartItem = []
-let itemPrices = []
+var cartItem = []
+var itemPrices = []
+var shippingFee = 5
+
+if (subTotal > 100){
+    shippingFee = "Free";
+} 
+
+var taxOwing === (subTotal * 1.12)
+
+if (cartItem == null){
+    document.write("Your Cart is Empty!")
+};
+    else {document.write("Your Subtotal is: " + "$" + subTotal + <br> + "+ Tax at 12% = " + taxOwing + <br> "+ Shipping = " + shippingFee + <hr> + "Your total = " + (subTotal + taxOwing + shippingFee)
+};
+
 
 function sum(itemPrices) {
     if (toString.call(itemPrices) !== "[object Array]")

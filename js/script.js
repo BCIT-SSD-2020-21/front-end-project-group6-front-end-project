b02bd41857f81f5f46552944d9f98f2671aae471
Yaroslav-Naft <71295049+Yaroslav-Naft@users.noreply.github.com>
2020-10-21 18:31:43 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// // Get Button HTML/CSS Info
+// GetRemoveButtonInfo = document.querySelectorAll('.btn-danger')
+// console.log(GetRemoveButtonInfo)
+
+// // For loop used to itterate through multiple buttons
+// for( i=0; i < GetRemoveButtonInfo.length; i++){
+//     var removeButton = GetRemoveButtonInfo[i];
+//     removeButton.addEventListener('click',function(){
+//         console.log("Button was clicked")
+
+//     });
+// }
+
+// $("button").click(function(){
+//     $(".btn-danger").css("color","blue");
+//     $("header").css("background-color", "green");
+//     console.log("Button was clicked")
+// });
+
+
+$(".btn-danger").click(function(){
+    $(".btn-danger").hide();
+
+
+ });
+
+

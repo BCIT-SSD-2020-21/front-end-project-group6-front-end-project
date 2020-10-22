45b9a57ce48807f0080f52f3fd8358d01257b271
Yaroslav-Naft <71295049+Yaroslav-Naft@users.noreply.github.com>
2020-10-21 21:11:34 -0700

    added default load settings

--- js/script.js
+++ js/script.js
@@ -1,32 +1,103 @@
-//Item Removal
+//Cart JS Script
+
+//Default Setting on load
+$( document ).ready(function() {
+//Remove Item-1
+    $(".hide-item-1").hide();
+    $(".hide-hr-1").hide();
+//Remove Item-2
+    $(".hide-item-2").hide();
+    $(".hide-hr-2").hide();
+ //Remove Item-3
+    $(".hide-item-3").hide();
+    $(".hide-hr-3").hide();
+ //Remove Item-4
+    $(".hide-item-4").hide();
+    $(".hide-hr-4").hide();
+//Remove Item-5
+    $(".hide-item-5").hide();
+    $(".hide-hr-5").hide();
+ //Remove Item-6
+    $(".hide-item-6").hide();
+    $(".hide-hr-6").hide();
+  });
+
+
+
+//Item addition for Cart page
+//Remove Item-1
+$(".add-btn-1").click(function(){
+    $(".show-item-1").hide();
+    $(".show-hr-1").show();
+ });
+
+//Remove Item-2
+$(".add-btn-2").click(function(){
+    $(".show-item-2").show();
+    $(".show-hr-2").show();
+ });
+
+ //Remove Item-3
+$(".add-btn-3").click(function(){
+    $(".show-item-3").show();
+    $(".show-hr-3").show();
+ });
+
+ //Remove Item-4
+$(".add-btn-4").click(function(){
+    $(".show-item-4").show();
+    $(".show-hr-4").show();
+ });
+
+//Remove Item-5
+$(".add-btn-5").click(function(){
+    $(".show-item-5").show();
+    $(".show-hr-5").show();
+ });
+
+ //Remove Item-6
+$(".add-btn-6").click(function(){
+    $(".show-item-6").show();
+    $(".show-hr-6").show();
+ });
+
+
+
+//Item removal for Cart page
 //Remove Item-1
 $(".rm-btn-1").click(function(){
     $(".hide-item-1").hide();
+    $(".hide-hr-1").hide();
  });
 
 //Remove Item-2
 $(".rm-btn-2").click(function(){
     $(".hide-item-2").hide();
+    $(".hide-hr-2").hide();
  });
 
  //Remove Item-3
 $(".rm-btn-3").click(function(){
     $(".hide-item-3").hide();
+    $(".hide-hr-3").hide();
  });
 
  //Remove Item-4
 $(".rm-btn-4").click(function(){
     $(".hide-item-4").hide();
+    $(".hide-hr-4").hide();
  });
 
 //Remove Item-5
 $(".rm-btn-5").click(function(){
     $(".hide-item-5").hide();
+    $(".hide-hr-5").hide();
  });
 
  //Remove Item-6
 $(".rm-btn-6").click(function(){
     $(".hide-item-6").hide();
+    $(".hide-hr-6").hide();
  });
 
 
@@ -43,4 +114,4 @@ $(".rm-btn-6").click(function(){
 
 
 
-//Make page default to no items until added in the shopping cart page
+

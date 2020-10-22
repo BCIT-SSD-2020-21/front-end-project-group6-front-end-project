fae347d9cc12efca61c1d2543bdd118aaaaa4c7e
Yaroslav-Naft <71295049+Yaroslav-Naft@users.noreply.github.com>
2020-10-21 21:41:17 -0700

    Added addition statement

--- js/script.js
+++ js/script.js
@@ -1,64 +1,64 @@
 //Cart JS Script
 
-//Default Setting on load
-$( document ).ready(function() {
-//Remove Item-1
-    $(".hide-item-1").hide();
-    $(".hide-hr-1").hide();
-//Remove Item-2
-    $(".hide-item-2").hide();
-    $(".hide-hr-2").hide();
- //Remove Item-3
-    $(".hide-item-3").hide();
-    $(".hide-hr-3").hide();
- //Remove Item-4
-    $(".hide-item-4").hide();
-    $(".hide-hr-4").hide();
-//Remove Item-5
-    $(".hide-item-5").hide();
-    $(".hide-hr-5").hide();
- //Remove Item-6
-    $(".hide-item-6").hide();
-    $(".hide-hr-6").hide();
-  });
+// //Default Setting on load
+// $( document ).ready(function() {
+// //Remove Item-1
+//     $(".hide-item-1").hide();
+//     $(".hide-hr-1").hide();
+// //Remove Item-2
+//     $(".hide-item-2").hide();
+//     $(".hide-hr-2").hide();
+//  //Remove Item-3
+//     $(".hide-item-3").hide();
+//     $(".hide-hr-3").hide();
+//  //Remove Item-4
+//     $(".hide-item-4").hide();
+//     $(".hide-hr-4").hide();
+// //Remove Item-5
+//     $(".hide-item-5").hide();
+//     $(".hide-hr-5").hide();
+//  //Remove Item-6
+//     $(".hide-item-6").hide();
+//     $(".hide-hr-6").hide();
+//   });
 
 
 
 //Item addition for Cart page
 //Remove Item-1
 $(".add-btn-1").click(function(){
-    $(".show-item-1").hide();
-    $(".show-hr-1").show();
+    $(".hide-item-1").show();
+    $(".hide-hr-1").show();
  });
 
 //Remove Item-2
 $(".add-btn-2").click(function(){
-    $(".show-item-2").show();
-    $(".show-hr-2").show();
+    $(".hide-item-2").show();
+    $(".hide-hr-2").show();
  });
 
  //Remove Item-3
 $(".add-btn-3").click(function(){
-    $(".show-item-3").show();
-    $(".show-hr-3").show();
+    $(".hide-item-3").show();
+    $(".hide-hr-3").show();
  });
 
  //Remove Item-4
 $(".add-btn-4").click(function(){
-    $(".show-item-4").show();
-    $(".show-hr-4").show();
+    $(".hide-item-4").show();
+    $(".hide-hr-4").show();
  });
 
 //Remove Item-5
 $(".add-btn-5").click(function(){
-    $(".show-item-5").show();
-    $(".show-hr-5").show();
+    $(".hide-item-5").show();
+    $(".hide-hr-5").show();
  });
 
  //Remove Item-6
 $(".add-btn-6").click(function(){
-    $(".show-item-6").show();
-    $(".show-hr-6").show();
+    $(".hide-item-6").show();
+    $(".hide-hr-6").show();
  });
 
 

0029321a92732a1ede883122e718b32aa31df879
Yaroslav-Naft <71295049+Yaroslav-Naft@users.noreply.github.com>
2020-10-21 19:19:49 -0700

    Added js for all removal and modified cart css

--- js/script.js
+++ js/script.js
@@ -9,11 +9,38 @@ $(".rm-btn-2").click(function(){
     $(".hide-item-2").hide();
  });
 
+ //Remove Item-3
+$(".rm-btn-3").click(function(){
+    $(".hide-item-3").hide();
+ });
+
+ //Remove Item-4
+$(".rm-btn-4").click(function(){
+    $(".hide-item-4").hide();
+ });
+
+//Remove Item-5
+$(".rm-btn-5").click(function(){
+    $(".hide-item-5").hide();
+ });
+
+ //Remove Item-6
+$(".rm-btn-6").click(function(){
+    $(".hide-item-6").hide();
+ });
+
+
+
+
+
+
+
+
 //Remove expected shipping delivery date
 //Research jquery for it
-
 
 //Adjust item count
 
 
 
+//Make page default to no items until added in the shopping cart page

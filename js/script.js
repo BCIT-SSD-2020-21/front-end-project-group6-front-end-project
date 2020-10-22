72e44789adfd7c28139e8761c7bec92adb713dc7
Yaroslav-Naft <71295049+Yaroslav-Naft@users.noreply.github.com>
2020-10-21 18:52:23 -0700

    added js removal

--- js/script.js
+++ js/script.js
@@ -1,22 +1,4 @@
-// // Get Button HTML/CSS Info
-// GetRemoveButtonInfo = document.querySelectorAll('.btn-danger')
-// console.log(GetRemoveButtonInfo)
-
-// // For loop used to itterate through multiple buttons
-// for( i=0; i < GetRemoveButtonInfo.length; i++){
-//     var removeButton = GetRemoveButtonInfo[i];
-//     removeButton.addEventListener('click',function(){
-//         console.log("Button was clicked")
-
-//     });
-// }
-
-// $("button").click(function(){
-//     $(".btn-danger").css("color","blue");
-//     $("header").css("background-color", "green");
-//     console.log("Button was clicked")
-// });
-
+//Item Removal
 //Remove Item-1
 $(".rm-btn-1").click(function(){
     $(".hide-item-1").hide();
@@ -26,5 +8,12 @@ $(".rm-btn-1").click(function(){
 $(".rm-btn-2").click(function(){
     $(".hide-item-2").hide();
  });
+
+//Remove expected shipping delivery date
+//Research jquery for it
+
+
+//Adjust item count
+
 
 

62cb6b4ee8d9044e3f092d933078fc7e781fe3fb
mandeep1580 <mandeep15@gmail.com>
2020-10-26 19:51:11 -0700

    updated book and script

--- js/script.js
+++ js/script.js
@@ -319,6 +319,7 @@ function bookSelect(books) {
 
 function InitCartPage() {
     let booksInCart = localStorage.getItem("bookSelected");
+
     //let book1 = $(".cart-container");
     let book1 = document.querySelector(".cart-container")
     booksInCart = JSON.parse(booksInCart)
@@ -326,23 +327,7 @@ function InitCartPage() {
 
     //book1.append("<div class="test"><span class="title">Hello World</span></div>");
 
-    function totalBookCost(books) {
 
-        //console.log("The cost is ", books.price);
-
-        let cartCost = localStorage.getItem('totalBookCost');
-
-        console.log("My cartCost is", cartCost);
-
-        if (cartCost != null) {
-            cartCost = parseInt(cartCost);
-
-            localStorage.setItem("totalBookCost", cartCost + books.price);
-        } else {
-            localStorage.setItem("totalBookCost", books.price);
-        }
-
-    }
 
     if (booksInCart) {
 
@@ -409,5 +394,23 @@ function InitCartPage() {
     // <span>${item.price}</span>  
     //  }
     //  )
+
+}
+
+function totalBookCost(books) {
+
+    //console.log("The cost is ", books.price);
+
+    let cartCost = localStorage.getItem('totalBookCost');
+
+    console.log("My cartCost is", cartCost);
+
+    if (cartCost != null) {
+        cartCost = parseInt(cartCost);
+
+        localStorage.setItem("totalBookCost", cartCost + books.price);
+    } else {
+        localStorage.setItem("totalBookCost", books.price);
+    }
 
 }
